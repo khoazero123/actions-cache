336f609e86184ec8d92f30bb669eb8b5b6c33c29
KhoaZero123 <khoazero123@gmail.com>
2022-05-06 10:34:59 +0700

    Remove check cache hit on save

--- dist/save/index.js
+++ dist/save/index.js
@@ -46334,10 +46334,6 @@ function run() {
                 utils.logWarning(`Error retrieving key from state.`);
                 return;
             }
-            if (utils.isExactKeyMatch(primaryKey, state)) {
-                core.info(`Cache hit occurred on the primary key ${primaryKey}, not saving cache.`);
-                return;
-            }
             const cachePaths = utils.getInputAsArray(constants_1.Inputs.Path, {
                 required: true
             });
