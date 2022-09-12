ffa8f0fbb44ecb0b8f909df451eeeef9b390561c
KhoaZero123 <khoazero123@gmail.com>
2022-09-12 08:34:12 +0700

    Fore save cache

--- src/save.ts
+++ src/save.ts
@@ -35,9 +35,9 @@ async function run(): Promise<void> {
 
         if (utils.isExactKeyMatch(primaryKey, state)) {
             core.info(
-                `Cache hit occurred on the primary key ${primaryKey}, not saving cache.`
+                `Cache hit occurred on the primary key ${primaryKey}, saving new cache.`
             );
-            return;
+            // return;
         }
 
         const cachePaths = utils.getInputAsArray(Inputs.Path, {
